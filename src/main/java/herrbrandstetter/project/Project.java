37eb9437b2bc5e3f81d3e2503a09473579f88e90
ranbrandstetter <ranolito9@icloud.com>
2021-06-13 14:29:00 +0200

    Initial commit

---
+++ src/main/java/herrbrandstetter/project/Project.java
@@ -0,0 +1,16 @@
+package herrbrandstetter.project;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("project")
+public class Project {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "project";
+
+    public Project() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
