b2c1e72d8849cedaae6be49ed02bce9d4eb1dc3c
ranbrandstetter <ranolito9@icloud.com>
2021-07-27 20:27:45 +0200

    Switch to Official + Parchment mappings and some other changes

--- src/main/java/herrbrandstetter/project/Project.java
+++ src/main/java/herrbrandstetter/project/Project.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod("project")
+@Mod(Project.MOD_ID)
 public class Project {
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "project";
